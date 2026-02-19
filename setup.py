f7e5622f4936c191263518d4e54792123f5fa520
LEGEND'S DaD <legendsdad001@gmail.com>
2026-02-19 13:43:44 +0530

    Added system

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, Extension
+import sys
 import pybind11
+
+# Detect compiler and set correct flags
+if sys.platform == "win32":
+    extra_compile_args = ["/O2"]
+else:
+    extra_compile_args = ["-O3"]
 
 ext_modules = [
     Extension(
@@ -7,13 +14,16 @@ ext_modules = [
         ["calculus_core.cpp"],
         include_dirs=[pybind11.get_include()],
         language="c++",
-        extra_compile_args=["/O2"]
-    )
+        extra_compile_args=extra_compile_args,
+    ),
 ]
 
 setup(
-    name="calculus",
+    name="calculus-cpp",
     version="0.1.0",
+    author="LegedsDaD",
+    description="High-performance calculus library using C++ backend",
     packages=["calculus"],
     ext_modules=ext_modules,
+    zip_safe=False,
 )
